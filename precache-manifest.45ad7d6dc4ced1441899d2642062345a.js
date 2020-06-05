874502ab17e5180b94669791847ae77b03938ba4
tony <serratedhumor@yahoo.com>
2020-06-05 17:14:58 -0500

    Updates

---
+++ precache-manifest.45ad7d6dc4ced1441899d2642062345a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb6f6058b783b5b74ff49ce075204a6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1422d9da7150d61e1534",
+    "url": "/static/css/main.f3857c99.chunk.css"
+  },
+  {
+    "revision": "f925a95b94d12b648fc9",
+    "url": "/static/js/2.6487f767.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6487f767.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1422d9da7150d61e1534",
+    "url": "/static/js/main.498c2924.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  }
+]);
